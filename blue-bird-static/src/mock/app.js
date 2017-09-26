7a5e4a839e0e8d76f4e79a3a0dbeca64d83188e7
戴斌 <daibin@ofpay.com>
2017-09-26 17:44:17 +0800

    前端登录

---
+++ blue-bird-static/src/mock/app.js
@@ -0,0 +1,74 @@
+const qs = require('qs')
+const Mock = require('mockjs')
+const config = require('../utils/config')
+
+const { apiPrefix, api } = config
+
+const { user} = api
+console.log('app mock ',user)
+
+const EnumRoleType = {
+  ADMIN: 'admin',
+  DEFAULT: 'guest',
+  DEVELOPER: 'developer',
+}
+
+const userPermission = {
+  DEFAULT: {
+    visit: ['1', '2', '21', '7', '5', '51', '52', '53'],
+    role: EnumRoleType.DEFAULT,
+  },
+  ADMIN: {
+    role: EnumRoleType.ADMIN,
+  },
+  DEVELOPER: {
+    role: EnumRoleType.DEVELOPER,
+  },
+}
+
+const adminUsers = [
+  {
+    id: 0,
+    username: 'admin',
+    password: 'admin',
+    permissions: userPermission.ADMIN,
+  }, {
+    id: 1,
+    username: 'guest',
+    password: 'guest',
+    permissions: userPermission.DEFAULT,
+  }, {
+    id: 2,
+    username: '吴彦祖',
+    password: '123456',
+    permissions: userPermission.DEVELOPER,
+  },
+]
+
+module.exports = {
+
+  [`GET ${apiPrefix}/user`] (req, res) {
+    const cookie = req.headers.cookie || ''
+    const cookies = qs.parse(cookie.replace(/\s/g, ''), { delimiter: ';' })
+    const response = {}
+    const user = {}
+    if (!cookies.token) {
+      res.status(200).send({ message: 'Not Login' })
+      return
+    }
+    const token = JSON.parse(cookies.token)
+    if (token) {
+      response.success = token.deadline > new Date().getTime()
+    }
+    if (response.success) {
+      const userItem = adminUsers.filter(_ => _.id === token.id)
+      if (userItem.length > 0) {
+        user.permissions = userItem[0].permissions
+        user.username = userItem[0].username
+        user.id = userItem[0].id
+      }
+    }
+    response.user = user
+    res.json(response)
+  },
+}
---
+++ blue-bird-static/src/services/app.js
@@ -0,0 +1,29 @@
+import { request, config } from 'utils'
+
+const { api } = config
+const { user, userLogout, userLogin } = api
+
+export async function login (params) {
+  return request({
+    url: userLogin,
+    method: 'post',
+    data: params,
+  })
+}
+
+export async function logout (params) {
+  return request({
+    url: userLogout,
+    method: 'get',
+    data: params,
+  })
+}
+
+export async function query (params) {
+  console.log('app',params,user)
+  return request({
+    url: user.replace('/:id', ''),
+    method: 'get',
+    data: params,
+  })
+}
